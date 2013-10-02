efe77658e54cecb1f3a268bcc95d206b30d09e48
janstenpickle <jansen.chris@gmail.com>
2013-10-02 14:57:31 +0100

    hope this still works...

--- etcd-docker.go
+++ etcd-docker.go
@@ -9,6 +9,23 @@ import (
 	"time"
 )
 
+func localIp() string {
+	var addr string
+
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatal("Oops: " + err.Error() + "\n")
+	} else {
+
+		for _, a := range addrs {
+			if ipnet, ok := a.(*net.IPNet); ok && ipnet.IP.IsGlobalUnicast() {
+				addr = ipnet.IP.String()
+			}
+		}
+	}
+	return addr
+}
+
 func inspectAndSet(c chan string, etcdClient *etcd.Client, dockerClient *docker.Client, keyname *string, ttl *uint64) {
 	for {
 		id := <-c
@@ -21,23 +38,13 @@ func inspectAndSet(c chan string, etcdClient *etcd.Client, dockerClient *docker.
 
 			//if the container config contains ports then set the IP as the host IP
 			if len(container.Config.PortSpecs) > 0 {
-				addrs, err := net.InterfaceAddrs()
-				if err != nil {
-					log.Fatal("Oops: " + err.Error() + "\n")
-				} else {
-
-					for _, a := range addrs {
-						if ipnet, ok := a.(*net.IPNet); ok && ipnet.IP.IsGlobalUnicast() {
-							addr = ipnet.IP.String()
-						}
-					}
-				}
+				addr = localIp()
 			} else {
 				addr = container.NetworkSettings.IPAddress
 			}
 
 			etcdClient.TestAndSet(*keyname+"/"+container.Config.Hostname, addr, addr, *ttl)
-			log.Print(id)
+			log.Print(container.Config.Hostname)
 
 		}
 
@@ -68,15 +75,15 @@ func main() {
 	ttl := flag.Uint64("ttl", 172800, "Time to live of the host entry")
 	dockerAPIPort := flag.String("port", "4243", "Docker API Port")
 	interval := flag.Duration("interval", 10, "Docker API to Etcd sync interval")
-	etcdHost := flag.String("etcd_host", "127.0.0.1", "Etcd host")
-	etcdPort := flag.String("etcd_port", "4001", "Etcd port")
-	concurrency := flag.Int("concurrency", 5, "Number of worker threads")
+	//etcdHost := flag.String("etcd_host", "127.0.0.1", "Etcd host")
+	//etcdPort := flag.String("etcd_port", "4001", "Etcd port")
+	concurrency := flag.Int("concurrency", 1, "Number of worker threads")
 
 	flag.Parse()
 
-	etcdCluster := []string{"http://" + *etcdHost + ":" + *etcdPort}
+	//etcdCluster := []string{"http://" + *etcdHost + ":" + *etcdPort}
 	etcdClient := etcd.NewClient()
-	etcdClient.SetCluster(etcdCluster)
+	//etcdClient.SetCluster(etcdCluster)
 
 	dockerClient, err := docker.NewClient("http://127.0.0.1:" + *dockerAPIPort)
 	if err != nil {
